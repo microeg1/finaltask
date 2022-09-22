46f09c024eb77f27d6177238e68cda171fc2659a
Gogol Egor <pro100egar@gmail.com>
2022-09-22 22:49:04 +0700

    SortArray add

--- FinalTask1/Program.cs
+++ FinalTask1/Program.cs
@@ -3,6 +3,18 @@
 
 class Program
 {
+    static void SortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
     static void Main(string[] args)
     {
         string[] array1 = new string[4] {"2", "hello", "world", ":-)"};
