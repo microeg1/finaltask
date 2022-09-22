c48a7866f57952fcae2dee1e5f2e67f2841861ea
Gogol Egor <pro100egar@gmail.com>
2022-09-22 22:47:25 +0700

    array add

--- FinalTask1/Program.cs
+++ FinalTask1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] array1 = new string[4] {"2", "hello", "world", ":-)"};
+        string[] array2 = new string[array1.Length];
+    }
+}
